63462fac89378d755df7c7d2b303459012431a01
Vivek Seth <Manu@Deepak-Seths-MacBook.local>
2012-05-18 00:21:34 -0400

    Initial Commit

---
+++ FileEncryptor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileEncryptor
+//
+//  Created by Vivek Seth on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
